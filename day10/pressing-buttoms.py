9095daf0e263a4d52fabc440c11326edc146f856
Andrew Gallant <andrew.m-ctr.gallant@faa.gov>
2025-12-10 15:19:24 -0600

    day 10, part 1

---
+++ day10/pressing-buttoms.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+import itertools
+
+# returns a list of true/false, after button is pressed
+def press_button(light, button):
+
+    for i in range(len(light)):
+        if i in button:
+            light[i] = not light[i]
+
+    return light
+
+
+# looks at one line, returns lowest button presses
+def pressing_some_buttons(line):
+
+    end = len(line) - 1
+
+    #break line into parts, lights is a list of true/false. buttons is a list of lists of ints
+    lights = [light == '#' for light in line[0].strip("[]")]
+    temp_buttons = []
+
+    for button in range(1, end):
+      temp_buttons.append(line[button])
+
+    buttons = [list(map(int, string.strip("()").split(","))) for string in temp_buttons]
+
+    # Using this in part 2, will come back to later
+    jolts = [jolt.strip("{}").split(",") for jolt in line[end].strip("{}").split(",")]
+
+    # now we have the parts we want
+
+    current_light = [False for i in range(len(lights))]
+    times_pressed = 0
+    best = 9999
+
+    for length in range(1, len(buttons) + 1):
+        for collection_of_buttons in itertools.combinations(buttons, length): # only looking at combitations bc order DOESN'T MATTER.... duh
+
+            current_light = [False for i in range(len(lights))]
+            times_pressed = 0
+
+            for button in collection_of_buttons:
+                current_light = press_button(current_light, button)
+                times_pressed += 1
+                if times_pressed > 20:
+                    print("left loop early")
+                    break
+
+            if current_light == lights:
+                if best > times_pressed:
+                    best = times_pressed
+
+
+    return best
+
+def main():
+
+    lines = []
+
+    with open("../puzzle-inputs/indicator-lights.txt") as f:
+        raw_lines = [ line.strip() for line in f if line.strip() ]
+
+        for raw_line in range(len(raw_lines)): 
+            lines.append((raw_lines[raw_line].split()))
+
+    total = 0
+    for line in lines:
+        print(line)
+        total = total + pressing_some_buttons(line)
+
+    print(total)
+
+main()
